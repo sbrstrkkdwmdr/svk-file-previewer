bf909b383e987584209b97a234fb186df296f343
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2025-11-16 01:37:41 +1100

    Update +layout.svelte

--- src/routes/public/+layout.svelte
+++ src/routes/public/+layout.svelte
@@ -6,15 +6,31 @@
     import { onMount } from "svelte";
     let { data, children } = $props();
     let pageMeta = $state(data.headmeta);
-    let colourMode = $state('dark_default');
+    let colourMode = $state("dark_default");
     onMount(() => {
         layoutUpdate();
     });
-    afterNavigate(() => {
-    });
+    afterNavigate(() => {});
     function layoutUpdate() {
         pageMeta = data.headmeta;
         colourMode = getColourMode();
+    }
+    function updateBodyClass(newtheme: string) {
+        const body = document.getElementsByTagName("body")[0];
+        if (!body) return;
+        let bClass = body.className;
+        const split = bClass.split(" ");
+        let themed = false;
+        for (let i = 0; i < split.length; i++) {
+            if (split[i].startsWith("theme-")) {
+                split[i] = "theme-" + newtheme;
+                themed = true;
+            }
+        }
+        if (!themed) {
+            split.push("theme-" + newtheme);
+        }
+        body.className = split.join(" ");
     }
     $effect(() => {
         try {
@@ -22,6 +38,7 @@
         } catch (err) {
             console.log(err);
         }
+        updateBodyClass(colourMode);
     });
 </script>
 
