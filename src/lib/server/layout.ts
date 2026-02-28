0ca0105bf1b624d2d877a2ec4e99981c08638d28
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2026-03-01 00:52:47 +1100

    fix file title in head being "folder preview"

--- src/lib/server/layout.ts
+++ src/lib/server/layout.ts
@@ -1,20 +1,25 @@
+import type { file } from "$lib/data/files";
 import { getMime } from "$lib/MIME";
 import { UrlParser } from "$lib/tools";
-import type { LayoutServerLoad } from "../../routes/public/$types";
-// import type { LayoutServerLoad } from 
+import type { LayoutServerLoad } from "../../routes/$types";
+import { files } from "./files";
+// import type { LayoutServerLoad } from
 export const load: LayoutServerLoad = async (ld) => {
     const parser = new UrlParser(ld.url.href);
-    let colourMode: colourMode = validateColourmode(ld.cookies.get('colourMode'));
-    if (!ld.cookies.get('colourMode')) ld.cookies.set('colourMode', 'dark_default', { path: '/' });
+    let colourMode: colourMode = validateColourmode(
+        ld.cookies.get("colourMode"),
+    );
+    if (!ld.cookies.get("colourMode"))
+        ld.cookies.set("colourMode", "dark_default", { path: "/" });
     return {
         headmeta: toMetaData(parser, ld.url),
-        colourMode
+        colourMode,
     };
 };
 
-type colourMode = 'dark_default' | 'light_default';
+type colourMode = "dark_default" | "light_default";
 
-const colourModes: colourMode[] = ['dark_default', 'light_default'];
+const colourModes: colourMode[] = ["dark_default", "light_default"];
 
 function validateColourmode(str: string | undefined): colourMode {
     if (str) {
@@ -22,28 +27,34 @@ function validateColourmode(str: string | undefined): colourMode {
             if (elem == str) return str;
         }
     }
-    return 'dark_default';
+    return "dark_default";
 }
 
 function toMetaData(parser: UrlParser, url: URL) {
-    const type = url.pathname.includes('.') ? 'file' : 'folder';
-    const path = url.pathname.split('/');
-    if (type == 'file') {
-        const fname = `File preview - ${path.slice().pop() ?? 'unknown'}`;
+    const type = url.pathname.startsWith("/open/") ? "file" : "folder";
+    const path = url.pathname.split("/");
+    if (type == "file") {
+        let tfile: file | null = null;
+        for (const sf of files ?? []) {
+            if (sf.hash == path[2]) {
+                tfile = sf;
+                break;
+            }
+        }
+        const fname = `File preview - ${tfile?.name ?? "unknown"}`;
         const mime = getMime(fname);
-return `<title>${fname}</title>
+        return `<title>${fname}</title>
 <meta content="${fname}" property="og:title" />
 <meta content="File preview - ${mime}" property="og:description" />
 <meta content="${parser.href}" property="og:url" />
 <meta content="${url.origin}/icons/favicon-64x64.png" property="og:image" />
 <meta content="#FF1921" data-react-helmet="true" name="theme-color" />`;
     }
-    const fname = `Folder preview - ${path.join('/') ?? 'unknown'}`;
-return `<title>${fname}</title>
+    const fname = `Folder preview - ${path.join("/") ?? "unknown"}`;
+    return `<title>${fname}</title>
 <meta content="${fname}" property="og:title" />
 <meta content="Folder preview" property="og:description" />
 <meta content="${parser.href}" property="og:url" />
 <meta content="${url.origin}/icons/favicon-64x64.png" property="og:image" />
 <meta content="#FF1921" data-react-helmet="true" name="theme-color" />`;
-
 }
