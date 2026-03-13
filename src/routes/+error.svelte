37ff0a698adbfa5ef69fe43bf21e1f1fd220f55d
SaberStrike <71379681+sbrstrkkdwmdr@users.noreply.github.com>
2026-03-14 01:50:59 +1100

    tweak error layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { page } from "$app/state";
+</script>
+
+<h1>ERROR {page.status}</h1>
+<div>{page.error!.message}</div>
+<br /><br />
+<div>
+    <a href="/">Return to home page</a>
+</div>
