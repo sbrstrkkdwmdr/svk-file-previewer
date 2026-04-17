c2da1fcae54ca1573f8ca40aed9d6f47432f9316
sbrstrkkdwmdr <sabersfear@gmail.com>
2026-04-17 14:41:38 +1000

    add line numbers to raw text preview

--- src/lib/renders/text-render.svelte
+++ src/lib/renders/text-render.svelte
@@ -1,11 +1,27 @@
 <script lang="ts">
+    import { browser } from "$app/environment";
     let { text } = $props();
 </script>
-<p>
-    {text}
-</p>
+<pre class="language-placeholder line-numbers">
+        <code class="language-placeholder">{text}</code>
+    </pre>
+<link href="/app/libs/prism.css" rel="stylesheet" />
+<!-- {#if colourMode == "light_default"} -->
+    <!-- <link href="https://prismjs.catppuccin.com/latte.css" rel="stylesheet" /> -->
+<!-- {:else} -->
+<!-- {/if} -->
+<!-- <link href="https://prismjs.catppuccin.com/mocha.css" rel="stylesheet" /> -->
+{#if browser}
+    <script src="/app/libs/prism.js"></script>
+    <script>
+        window.Prism = window.Prism || {};
+        Prism.disableWorkerMessageHandler = true;
+        prism.highlightAll();
+    </script>
+{/if}
+
 <style>
-    p {
+    pre {
         border: 1px solid var(--border);
         border-radius: 0px;
     }
